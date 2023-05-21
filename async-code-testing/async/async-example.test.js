e327ec6206ad40424a8acdc549821f82ddf48a49
Arjan Hoek <132347615+ArjanHoek-Topicus@users.noreply.github.com>
2023-05-21 10:29:05 +0200

    add async code testing files

---
+++ async-code-testing/async/async-example.test.js
@@ -0,0 +1,35 @@
+import { describe, expect, it } from "vitest";
+import { generateToken, generateTokenPromise } from "./async-example";
+
+describe(generateToken.name, () => {
+    it("should generate a token value", (done) => {
+        const testUserEmail = "test@test.com";
+
+        generateToken(testUserEmail, (_, token) => {
+            try {
+                expect(token).toBeDefined();
+                // expect(token).toBe(2);
+                done();
+            } catch (err) {
+                done(err);
+            }
+        });
+    });
+
+    it("should generate a token value", () => {
+        const testUserEmail = "test@test.com";
+
+        return expect(
+            generateTokenPromise(testUserEmail)
+        ).resolves.toBeDefined();
+    });
+
+    it("should generate a token value", async () => {
+        const testUserEmail = "test@test.com";
+
+        const token = await generateTokenPromise(testUserEmail);
+
+        expect(token).toBeDefined();
+        expect(token).not.toBeTypeOf("number");
+    });
+});

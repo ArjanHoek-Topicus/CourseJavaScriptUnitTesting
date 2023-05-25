49571d279287ff850508fcc95f80a29c063d5eb6
Arjan Hoek <132347615+ArjanHoek-Topicus@users.noreply.github.com>
2023-05-25 13:26:28 +0200

    add file system mock

--- mocking-spies/src/util/io.test.js
+++ mocking-spies/src/util/io.test.js
@@ -1,13 +1,20 @@
-import { it, expect, describe } from "vitest";
+import { it, expect, describe, vi } from "vitest";
 import writeData from "./io";
+import { promises as fs } from "fs";
+
+vi.mock("fs");
 
 describe(writeData.name, () => {
     it("should execute the writeFile method", () => {
         const testData = "test";
         const testFileName = "test.txt";
 
-        return expect(
-            writeData(testData, testFileName)
-        ).resolves.toBeUndefined();
+        // return expect(
+        //     writeData(testData, testFileName)
+        // ).resolves.toBeUndefined();
+
+        writeData(testData, testFileName);
+
+        expect(fs.writeFile).toHaveBeenCalled();
     });
 });

f8d8b7462f49a8157f8ae14a5737595cdf2b6913
Arjan Hoek <132347615+ArjanHoek-Topicus@users.noreply.github.com>
2023-05-26 15:18:44 +0200

    add last test and finish course

--- more-mocking/util/dom.test.js
+++ more-mocking/util/dom.test.js
@@ -15,24 +15,29 @@ const document = window.document;
 vi.stubGlobal("document", document);
 
 describe(showError.name, () => {
+    let errorsEl;
+
     beforeEach(() => {
         document.body.innerHTML = "";
         document.write(htmlDocumentContent);
+        errorsEl = document.getElementById("errors");
     });
 
     it("should add an error paragraph to the errors container element", () => {
         showError("This is not working");
-
-        const errorsEl = document.getElementById("errors");
         const errorParagraph = errorsEl.firstElementChild;
-
         expect(errorParagraph).not.toBeNull();
     });
 
     it("should not contain an error paragraph initially", () => {
-        const errorsEl = document.getElementById("errors");
         const errorParagraph = errorsEl.firstElementChild;
-
         expect(errorParagraph).toBeNull();
+    });
+
+    it("should output the provided message in the error paragraph", () => {
+        const errorMessage = "This is not working...";
+        showError(errorMessage);
+        const errorParagraph = errorsEl.firstElementChild;
+        expect(errorParagraph.textContent).toBe(errorMessage);
     });
 });
